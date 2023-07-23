775d46f505b47ae54e8d7558c327d1c2ed16b50a
massanengineer <matsuda4645@gmail.com>
2023-07-23 20:15:31 +0900

    deviseでユーザー認証を実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address profile])
+  end
+
+  def after_sign_out_path_for
+    new_user_session_path # ログアウト後の遷移先を指定
+  end
 end
