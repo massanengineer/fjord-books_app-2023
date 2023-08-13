fdf37c6b0ad57182768449c88e0c96c1adf03c93
massanengineer <matsuda4645@gmail.com>
2023-08-14 02:20:20 +0900

    save!に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
-    @comment.save
+    @comment.save!
     redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
 
