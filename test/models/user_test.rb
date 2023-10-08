417816347a2d47b8957ab673f8726ae1daa3b2a4
massanengineer <matsuda4645@gmail.com>
2023-10-08 14:50:32 +0900

    Userクラスのテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alice)   
+  end
+  
+  test 'name_or_email' do
+
+    assert_equal 'alice', @user.name_or_email 
+    
+    @user.name = ''
+    assert_equal 'alice@example.com', @user.name_or_email
+
+    end
 end
